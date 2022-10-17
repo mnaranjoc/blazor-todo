933128b22faae392f7e585e08f359161f0ca1d63
Manuel Naranjo <manuelnaranjo@outlook.com>
2022-10-17 18:41:02 -0500

    add todolist and project template

---
+++ ToDoApp/Data/TodoList.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Data
+{
+    public class TodoList
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name is too long.")]
+        public string? Name { get; set; }
+    }
+}
---
+++ ToDoApp/Services/TodoListService.cs
@@ -0,0 +1,18 @@
+﻿using ToDoApp.Data;
+
+namespace ToDoApp.Services
+{
+    public class TodoListService
+    {
+        private List<TodoList> TodoLists { get; set; }
+
+        public TodoListService()
+            => TodoLists = new();
+
+        public void Create(TodoList newList)
+            => TodoLists.Add(newList);
+
+        public IEnumerable<TodoList> GetAll()
+            => TodoLists;
+    }
+}
